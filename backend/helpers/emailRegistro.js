69471163b1c8f7cac4f37866fd478d41eca389e5
Jose <jlaresti93@gmail.com>
2024-05-28 10:00:30 +0200

    Terminando implementacion de mandar email con mailtrap

--- backend/helpers/emailRegistro.js
+++ backend/helpers/emailRegistro.js
@@ -1,6 +1,6 @@
 import nodemailer from "nodemailer";
 
-const emailRegistro = () => {
+const emailRegistro = async (datos) => {
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
@@ -9,6 +9,23 @@ const emailRegistro = () => {
       pass: process.env.EMAIL_PASS,
     },
   });
+  const {email, nombre, token} = datos;
+
+  const info = await transporter.sendMail({
+    from: 'APV - Administrador de Pacientes de Veterinaria',
+    to: email, 
+    subject: 'Confirmar cuenta en APV',
+    text: 'Confrima la cuenta para poder iniciar sesión',
+    html: `
+        <p>Hola ${nombre} confirma tu cuenta en APV.</p>
+        <p>Tu cuenta esta lista, pulsa el siguiente enlace para activarla:  <a href="${process.env.FRONTEND_URL}/confirmar/${token}">CONFIRMAR CUENTA</a> </p>
+        <p>Si tu no creaste esta cuenta, ignora este mensaje</p> 
+    `
+  });
+
+  console.log("Mensaje enviado: %s", info.messageId);
+
 };
+
 
 export default emailRegistro;
