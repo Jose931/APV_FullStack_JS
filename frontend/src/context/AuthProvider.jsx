7a03e18e45347a2a618157dd1f3b05a6d1b84557
Jose <jlaresti93@gmail.com>
2024-06-05 20:55:22 +0200

    Autenticacion por JWT

--- frontend/src/context/AuthProvider.jsx
+++ frontend/src/context/AuthProvider.jsx
@@ -1,9 +1,36 @@
 import { useState, useEffect, createContext } from "react";
+import clienteAxios from "../config/axios.jsx";
 
 const AuthContext = createContext();
 
 const AuthProvider = ({children}) => {
     const [auth, setAuth] = useState({});
+
+    useEffect(() => {
+      const autenticarUsuario = async() => {
+        const token = localStorage.getItem('token');
+
+        if(!token) return;
+
+        const config =  {
+           headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`
+           } 
+        }
+
+        try {
+            const { data } = await clienteAxios('/veterinarios/perfil', config);
+
+            setAuth(data);
+        } catch (error) {
+            console.log(error.response.data.msg);
+            setAuth({});
+        }
+      }
+      autenticarUsuario();
+    }, [])
+    
 
     return (
         <AuthContext.Provider
